0be9ef68fc19fa42eceaacb0fc181b4c18249a02
guyeonjun2 <guyeonjun909@gmail.com>
2026-03-02 01:19:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -229,7 +229,9 @@ async def on_ready():
     global panel_message, previous_premium
     print("봇 준비 완료")
 
-    bot.add_view(PanelView())
+    class PanelView(View):
+    def __init__(self):
+        super().__init__(timeout=None)
 
     channel = await bot.fetch_channel(PANEL_CHANNEL_ID)
     premium, rate = get_kimchi()
